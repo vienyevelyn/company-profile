332f071bc0859e558a0d5fb3edf9ef06f3e335ea
vienyevelyn <vieny.evelin@gmail.com>
2025-08-04 13:29:49 +0700

    setting up serivec  page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,7 +7,27 @@ function Services(){
 
     return (
         <>
-        
+        <div>
+            <div>
+                <h1>
+                    <span>Our Company</span>
+                    <span>Services</span>
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptatibus, modi inventore aliquid non ratione? Corporis suscipit consectetur vel aperiam.</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>LOREM</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>IPSUM</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h2>DOLORES</h2>
+            </div>
+        </div>
         </>
     )
 
