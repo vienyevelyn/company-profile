435b1397ae1b352dafe0e1fd7fa6b95d1519738e
vienyevelyn <vieny.evelin@gmail.com>
2025-08-04 15:39:26 +0700

    Designing About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import about1 from "../assets/about1.jpg"
+import about2 from "../assets/about2.jpg"
+// import { useNavigate, Link } from "react-router-dom";
 
 
 function About(){
 
     return (
         <>
-        <div className="bg-blue-500">
-            <div>
-            <p>We are OneTech</p>
-            <h1>Set Up To build</h1>
-            <h2>A better way to Innovate</h2>
-            </div>
-            <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita odit quidem aliquam tenetur nisi praesentium, illum quae dolorum labore porro.</p>
-
-            </div>
-            
-            <div className="image">
-
-            </div>
-
-            <div>
-                <div>
-                    <h2>Our Story</h2>
-                    <img src="" alt="" />
+        <div className="w-[1500px] pt-36 flex flex-col gap-y-20 items-center justify-center px-20">
+            <div className="flex items-end justify-around gap-x-24 px-20">
+                <div className="text-start">
+                    <p className="text-lg text-blue-800 font-semibold">We are OneTech</p>
+                    <h1 className="text-5xl font-bold">Set Up To build</h1>
+                    <h2 className="text-5xl font-semibold text-gray-500">A better way to Innovate</h2>
                 </div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolorum iste consectetur asperiores expedita praesentium non mollitia esse nemo impedit.</p>
+                    <p className=" text-end text-md">No Responsive amet consectetur adipisicing elit. Pariatur dolor blanditiis veritatis odio facilis deleniti!.</p>
+                </div>
+            </div>
+            
+            <div className="flex justify-center items-center gap-x-10">
+                <img src={about1} alt="" className="w-full h-[400px] object-cover rounded-xl object-center shadow-md"/>
+                <img src={about2} alt="" className="w-full h-[400px] object-cover rounded-xl object-center shadow-md" />
+            </div>
+
+            <div className="flex justify-between gap-x-36 items-center mt-10">
+                <div>
+                    <h2 className="text-5xl font-bold text-blue-800 mb-5">Our Story</h2>
+                    <img src={about2} alt="" className="w-[500px] object-cover rounded-xl object-center shadow-md"/>
+                </div>
+                <div>
+                    <p className="text-end text-md max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolorum iste consectetur asperiores expedita praesentium non mollitia esse nemo impedit.</p>
                 </div>
             </div>
 
