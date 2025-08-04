e2e4fb5e45c9193aaeeb97cb181824d4332b2548
vienyevelyn <vieny.evelin@gmail.com>
2025-08-04 13:12:53 +0700

    setting up home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,11 @@ function Home(){
     return (
         <>
         <div className="bg-blue-50 w-full h-full">
-            
+            <h1>World's</h1>
+            <h1>One and Only</h1>
+            <h1>Finest AI</h1>
+            <p>by industry leaders worldwide. Pushing boundaries with Innovationg</p>
+            <button><Link to={"/about"}>About Us</Link></button>
         </div>
         </>
     )
