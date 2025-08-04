f14e30e37a78ee761ed6065048345611a221050a
vienyevelyn <vieny.evelin@gmail.com>
2025-08-04 21:35:09 +0700

    Home + About Responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,17 +10,17 @@ function About(){
 
     return (
         <>
-        <div className="w-[1500px] pt-36 mt-20 flex flex-col gap-y-20 items-center justify-center px-20">
-            <div className="flex items-end justify-around gap-x-24 px-20">
+        <div className="2xl:w-[1500px] xl:w-[1440px] lg:w-[1024px] w-full pt-36 mt-20 flex flex-col  gap-y-20 items-center justify-center px-20">
+            <div className="flex items-end lg:flex-row flex-col justify-around gap-x-24 px-20">
                 <motion.div
                 initial={{ opacity: 0, x: -50 }}
                 whileInView={{ opacity: 1, x: 0 }}
                 transition={{ duration: 1 }}
                 className="text-start"
                 >
-                    <p className="text-lg text-blue-800 font-semibold">We are OneTech</p>
-                    <h1 className="text-5xl font-bold">Set Up To build</h1>
-                    <h2 className="text-5xl font-semibold text-gray-500">A better way to Innovate</h2>
+                    <p className="lg:text-lg text-2xl text-blue-800 font-semibold">We are OneTech</p>
+                    <h1 className="lg:text-5xl text-7xl font-bold">Set Up To build</h1>
+                    <h2 className="lg:text-5xl text-7xl font-semibold text-gray-500">A better way to Innovate</h2>
                 </motion.div>
                 <motion.div
                 initial={{ opacity: 0, x: 60 }}
@@ -28,22 +28,22 @@ function About(){
                 transition={{ duration: 1 }}
                 className="text-start"
                 >
-                    <p className=" text-end text-md">No Responsive amet consectetur adipisicing elit. Pariatur dolor blanditiis veritatis odio facilis deleniti!.</p>
+                    <p className="md:text-end text-lg text-md">No Responsive amet consectetur adipisicing elit. Pariatur dolor blanditiis veritatis odio facilis deleniti!.</p>
                 </motion.div>
             </div>
             
-            <div className="flex animate-fade-up duration-3000 justify-center items-center gap-x-10">
-                <img src={about1} alt="" className="w-full h-[400px] object-cover rounded-xl object-center shadow-md"/>
-                <img src={about2} alt="" className="w-full h-[400px] object-cover rounded-xl object-center shadow-md" />
+            <div className="flex lg:flex-row flex-col animate-fade-up duration-3000 justify-center items-center lg:gap-x-10 gap-x-5 lg:gap-y-0 gap-y-10">
+                <img src={about1} alt="" className="xl:w-full md:w-full  lg:w-[300px] h-[400px] object-cover rounded-xl object-center shadow-md"/>
+                <img src={about2} alt="" className="xl:w-full md:w-full lg:w-[400px] h-[400px] object-cover rounded-xl object-center shadow-md" />
             </div>
 
-            <div className="flex animate-fade-up duration-3000 justify-between gap-x-36 items-center mt-10">
+            <div className="flex lg:flex-row flex-col animate-fade-up duration-3000 justify-between lg:gap-x-36 md:gap-y-10 items-center mt-10">
                 <div>
                     <h2 className="text-5xl font-bold text-blue-800 mb-5">Our Story</h2>
                     <img src={about2} alt="" className="w-[500px] object-cover rounded-xl object-center shadow-md"/>
                 </div>
                 <div>
-                    <p className="text-end text-md max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolorum iste consectetur asperiores expedita praesentium non mollitia esse nemo impedit.</p>
+                    <p className="md:text-end text-center text-md max-w-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolorum iste consectetur asperiores expedita praesentium non mollitia esse nemo impedit.</p>
                 </div>
             </div>
 
