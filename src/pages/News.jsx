4d76e271813eabad4d9dd21f2d27bf2dfd4c3578
vienyevelyn <vieny.evelin@gmail.com>
2025-08-04 18:12:21 +0700

    Designing News page

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -5,9 +5,10 @@ import axios from "axios";
 
 
 function News(){
-        const key = "157d69aa1dfd9f445f625e11a8634850"
-        const url = `https://gnews.io/api/v4/top-headlines?category=general&apikey=${key}`
-        const[data, setData] = useState([])
+        const key = "6b0cdf861729b996b5d6ef9ffb83a488"
+        const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`
+        const[datas, setData] = useState([])
+        const[error, setError] = useState("")
 
         async function fetchData(){
         try {
@@ -15,6 +16,7 @@ function News(){
 
             setData(response.data.results || [])
             setError("")
+            console.log(response.data)
 
         } 
         catch(err){
@@ -29,13 +31,28 @@ function News(){
     return (
 
         <>
-        <div>
-            <div className="bg-green-50">
-                <h4>OneTech</h4>
-                <h1>Article and News</h1>
+        <div className="mt-48 mb-40 mx-auto w-[1520px]">
+            <div className="bg-green-700 w-[1000px] h-[200px] mx-auto py-10 rounded-xl mb-20">
+                <h4 className="text-xl text-white font-semibold mb-2">(template API)</h4>
+                <h1 className="text-5xl text-white font-bold">Article and News</h1>
             </div>
-            <div className="news-api">
-                
+            {error && <p className="text-center text-red-400 text-lg mb-6">{error}</p>}
+
+            <div className="grid grid-cols-3 gap-20 mx-20 my-10">
+                {datas.map((data, index)=>(
+                    <div key={data.id} className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg overflow-hidden shadow-md hover:scale-110 transition duration-300 border border-white/20">
+
+
+                        <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+                        className="w-full"/>
+
+
+                        <div className="p-4">
+                            <h2 className="text-lg font-bold">{data.title}</h2>
+                        </div>
+                    </div>
+                    
+                ))}
             </div>
         </div>
         </>
