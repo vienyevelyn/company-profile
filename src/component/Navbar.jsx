bb917b5ae4e70cc8dba05549c227f66c8bfea8ed
vienyevelyn <vieny.evelin@gmail.com>
2025-08-04 14:54:02 +0700

    Designing Home page + Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,14 +6,14 @@ function Navbar(){
 
     return (
         <>
-        <div className="bg-red-50 w-full drop-shadow-md flex justify-between items-center px-6 py-4">
-            <h1 className="text-xl font-bold">OneTech</h1>
-            <ul className="flex space-x-6 font-medium text-gray-700">
+        <div className="w-[1100px] mx-auto fixed top-0 left-0 right-0 z-10 px-10 py-5  drop-shadow-xl flex justify-between items-center my-9 rounded-full bg-white">
+            <h1 className="text-2xl font-bold text-blue-800">OneTech</h1>
+            <ul className="flex space-x-6 font-medium text-blue-800">
                 <li><Link to={"/"}>Home</Link></li>
                 <li><Link to={"/about"}>About</Link></li>
                 <li><Link to={"/services"}>Services</Link></li>
                 <li><Link to={"/news"}>News</Link></li>
-                <li><Link to={"/Contact"}>Contact</Link></li>
+                <li><Link to={"/Contact"} className="bg-blue-800 py-2 px-3 rounded-full text-white">Contact</Link></li>
             </ul>
         </div>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import bg from "../assets/home-bg.jpg"
 
 
 function Home(){
 
     return (
         <>
-        <div className="bg-blue-50 w-full h-full">
-            <h1>World's</h1>
-            <h1>One and Only</h1>
-            <h1>Finest AI</h1>
-            <p>by industry leaders worldwide. Pushing boundaries with Innovationg</p>
-            <button><Link to={"/about"}>About Us</Link></button>
+        <div className="relative w-screen h-screen bg-cover bg-center pt-20"  style={{ backgroundImage: `url(${bg})`  }}>
+            <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+            <div className="absolute mx-auto text-start px-20 py-24 mt-20 left-0 right-0 w-[1300px] flex border gap-x-60 justify-center items-end rounded-xl border-white/80">
+                <div className="shrink-0">
+                    <h1 className="text-7xl font-bold text-white">World's</h1>
+                    <h1 className="text-5xl font-bold text-white/70">One and Only</h1>
+                    <h1 className="text-7xl font-bold text-white">Finest AI</h1>
+                </div>
+                <div className="text-end">
+                    <p className="mb-4 text-white font-semibold">No Responsive dolor amet consectetur adipisicing elit. Quidem voluptates esse eum reiciendis nisi necessitatibus debitis, nobis a consectetur voluptatem.</p>
+                    <button><Link to={"/about"} className="bg-blue-800 py-2 px-3 rounded-full text-white">About Us</Link></button>
+                </div>
+            </div>
+            
         </div>
         </>
     )
